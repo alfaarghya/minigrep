8a5d7fe02892a9449030d923e4b47e704eea84cc
alfaarghya <alfaarghya.dev@gmail.com>
2025-08-29 20:22:05 +0530

    read arguments & file 📖

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Let's build a minimal grep tool!");
+    // passing arguments: cargo run -- test files/poem.txt
+    let args: Vec<String> = env::args().collect();
+
+    // query and file path from arguments
+    let query: &String = &args[1];
+    let file_path: &String = &args[2];
+
+    println!("Query => {query}");
+    println!("In File: {file_path}");
+
+    // read the file
+    let content = fs::read_to_string(file_path)
+        .expect("Should be able to read the file");
+
+    println!("text: \n{content}");
+
 }
