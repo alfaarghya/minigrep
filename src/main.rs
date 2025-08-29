70c1ca54c7d5d000b89cc03a12263da8cc7d4189
alfaarghya <alfaarghya.dev@gmail.com>
2025-08-29 20:43:09 +0530

    restructure code & better error handling

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,34 @@ fn main() {
     // passing arguments: cargo run -- test files/poem.txt
     let args: Vec<String> = env::args().collect();
 
-    // query and file path from arguments
-    let query: &String = &args[1];
-    let file_path: &String = &args[2];
+    let config = Config::new(&args);
 
-    println!("Query => {query}");
-    println!("In File: {file_path}");
+    println!("Query => {}", config.query);
+    println!("In File: {}", config.file_path);
 
     // read the file
-    let content = fs::read_to_string(file_path)
+    let content = fs::read_to_string(config.file_path)
         .expect("Should be able to read the file");
 
     println!("text: \n{content}");
 
+}
+
+
+struct Config {
+    query: String,
+    file_path: String
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!(">> Not Enough Arguments!\n> example: cargo run -- test files/test.txt");
+        }
+
+        let query: String = args[1].clone();
+        let file_path: String = args[2].clone();
+
+        Config {query, file_path}
+    }
 }
