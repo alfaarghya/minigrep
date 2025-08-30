fd54bc477e8d15bae1e247432bd3942a4978185d
alfaarghya <alfaarghya.dev@gmail.com>
2025-08-31 00:09:57 +0530

    match pattern in multiple files

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,8 @@
+use std::collections::HashSet;
+
 pub struct Config {
     pub pattern: String,
-    pub file_path: String,
+    pub file_paths: Vec<String>,
     pub case_insensitive: bool,
 }
 
@@ -9,7 +11,7 @@ impl Config {
      * Build Config with arguments
      */
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        // at-least 2 arguments needed
+        //Corner Case => Not Enough Arguments
         if args.len() < 3 {
             return Err(">> Not Enough Arguments!\n\
                  > example: cargo run -- test files/test.txt\n\
@@ -17,18 +19,26 @@ impl Config {
         }
 
         let pattern: String = args[1].clone();
-        let file_path: String = args[2].clone();
+        //handle duplicate file paths with HashSet
+        let mut file_paths: HashSet<String> = HashSet::new();
         let mut case_insensitive: bool = false;
 
-        for arg in &args[1..] {
+        for arg in &args[2..] {
             if arg == "-i" {
                 case_insensitive = true;
+            } else {
+                file_paths.insert(arg.clone());
             }
+        }
+
+        // Corner Case => No file paths
+        if file_paths.is_empty() {
+            return Err(">> No file(s) provided!");
         }
 
         Ok(Config {
             pattern,
-            file_path,
+            file_paths: file_paths.into_iter().collect(), //convert into vector
             case_insensitive,
         })
     }
--- src/run.rs
+++ src/run.rs
@@ -2,12 +2,21 @@ use crate::{config::Config, grep};
 use std::{error::Error, fs};
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    // read the file
-    let contents = fs::read_to_string(config.file_path)?;
+    for file_path in config.file_paths {
+        // read the file
+        let contents: String = fs::read_to_string(&file_path)?;
 
-    // print the match pattern
-    for line in grep::search(&config.pattern, &contents, config.case_insensitive) {
-        println!("{line}");
+        // all match lines
+        let results: Vec<&str> = grep::search(&config.pattern, &contents, config.case_insensitive);
+
+        // results empty -> no matches on display
+        if !results.is_empty() {
+            // result available -> display matches on screen
+            println!("--- Matches in {file_path} ---");
+            for line in results {
+                println!("{line}");
+            }
+        }
     }
 
     Ok(())
